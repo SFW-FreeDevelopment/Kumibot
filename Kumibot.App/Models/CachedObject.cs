bd063d6fa2c0b322c6c6e335618dfb0766e7437e
Steven Garberg <steven.garberg.jr@gmail.com>
2022-05-05 22:05:31 -0400

    Scaffolding cached repository

---
+++ Kumibot.App/Models/CachedObject.cs
@@ -0,0 +1,13 @@
+﻿namespace Kumibot.App.Models;
+
+public class CachedObject
+{
+    public DateTime CachedAt { get; set; }
+    public object Data { get; set; }
+
+    public CachedObject(object data)
+    {
+        Data = data;
+        CachedAt = DateTime.Now;
+    }
+}
---
+++ Kumibot.App/Repositories/SportRadarRepository.cs
@@ -0,0 +1,27 @@
+﻿using Kumibot.App.Models;
+using Kumibot.App.Models.SportRadar;
+
+namespace Kumibot.App.Repositories;
+
+public class SportRadarRepository
+{
+    /// Cache time in minutes
+    private const int CacheTime = 1440;
+    
+    private static readonly IDictionary<string, CachedObject> _cache = new Dictionary<string, CachedObject>();
+
+    public async Task<Champions.Root> GetChampions()
+    {
+        if (_cache.ContainsKey(nameof(Champions)) &&
+            _cache[nameof(Champions)].CachedAt < DateTime.Now.AddMinutes(CacheTime))
+        {
+            return _cache[nameof(Champions)].Data as Champions.Root;
+        }
+        else
+        {
+            var data = await Task.FromResult(new object()); // TODO: Client call goes ere
+            _cache[nameof(Champions)] = new CachedObject(data);
+            return _cache[nameof(Champions)].Data as Champions.Root;
+        }
+    }
+}
