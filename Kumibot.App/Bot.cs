1f94c34879dd8551feba1c39439e2090ee52e6c6
Christopher Plummer <chrisplummer0194@gmail.com>
2022-05-05 20:40:42 -0400

    Project init

---
+++ Kumibot.App/Bot.cs
@@ -0,0 +1,74 @@
+﻿using System.Reflection;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kumibot.App;
+
+public static class Bot
+{
+    private static DiscordSocketClient _client;
+    private static CommandService _commands;
+    private static IServiceProvider _services;
+    
+
+    public static async Task RunBotAsync()
+    {
+        _client = new DiscordSocketClient();
+        _commands = new CommandService();
+        _services = new ServiceCollection()
+            .AddSingleton(_client)
+            .AddSingleton(_commands)
+            .BuildServiceProvider();
+
+        const string token = $"{Constants.BotToken}";
+
+        _client.Log += Log;
+
+        await RegisterCommandsAsync();
+
+        await _client.LoginAsync(TokenType.Bot, token);
+
+        await _client.StartAsync();
+
+        await Task.Delay(-1);
+    }
+
+    private static Task Log(LogMessage arg)
+    {
+        Console.WriteLine(arg);
+        return Task.CompletedTask;
+    }
+        
+    private static async Task RegisterCommandsAsync()
+    {
+        _client.MessageReceived += HandleCommandAsync;
+        await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+    }
+
+    private static async Task HandleCommandAsync(SocketMessage socketMessage)
+    {
+        var message = socketMessage as SocketUserMessage;
+        var context = new SocketCommandContext(_client, message);
+        if (message is null || message.Author.IsBot) return;
+
+        int argPos = 0;
+        if (message.HasStringPrefix("kumibot ", ref argPos))
+        {
+            var result = await _commands.ExecuteAsync(context, argPos, _services);
+            if (!result.IsSuccess)
+                Console.WriteLine(result.ErrorReason);
+        }
+        else
+        {
+            argPos = 0;
+            if (message.HasStringPrefix("<@961429965570719746> ", ref argPos))
+            {
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+                if (!result.IsSuccess)
+                    Console.WriteLine(result.ErrorReason);
+            }
+        }
+    }
+}
---
+++ Kumibot.App/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace Kumibot.App;
+
+public class Constants
+{
+    public const string BotToken = "CHANGE_ME";
+    
+    public static readonly string HelpMessage = $"**The following commands can be used:**{Environment.NewLine}" +
+                                                $"  • **ping** - Pings the Discord channel{Environment.NewLine}";
+    
+    public const string PingMessage = "I am pinging the server.";
+}
