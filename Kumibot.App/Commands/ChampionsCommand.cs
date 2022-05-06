31a7269f884262aed39b6bd9c3cbe60fa4587aed
Christopher Plummer <chrisplummer0194@gmail.com>
2022-05-05 22:55:52 -0400

    ChampionsCommand

---
+++ Kumibot.App/Commands/ChampionsCommand.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System.Text;
+using Discord.Commands;
+using Kumibot.App.Repositories;
+
+namespace Kumibot.App.Commands;
+
+public class ChampionsCommand : CommandBase
+{
+    private SportRadarRepository _repository;
+    
+    public ChampionsCommand(SportRadarRepository repository)
+    {
+        _repository = repository;
+    }
+    
+    [Command("champions")]
+    public async Task HandleCommandAsync()
+    {
+        var root = await _repository.GetChampions();
+        var weightClasses = root?.Categories?.FirstOrDefault(c => c.Name.Equals("UFC"))?.WeightClasses;
+        var sb = new StringBuilder();
+        var cultureInfo   = Thread.CurrentThread.CurrentCulture;  
+        var textInfo = cultureInfo.TextInfo;
+        if (weightClasses != null)
+            foreach (var weightClass in weightClasses)
+            {
+                sb.Append($"- **{textInfo.ToTitleCase(weightClass.Description)}**: {weightClass.Competitor.Name}\n");
+            }
+
+        await ReplyAsync($"**Champion List**:\n{sb}");
+    }
+}
