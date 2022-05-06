22cdabdb9641e87b8ae38f62a4afc0059373e9c3
Steven Garberg <steven.garberg.jr@gmail.com>
2022-05-05 22:35:34 -0400

    Added JsonPropertyName attribute to Champions model

--- Kumibot.App/Models/SportRadar/Champions.cs
+++ Kumibot.App/Models/SportRadar/Champions.cs
@@ -1,41 +1,72 @@
-﻿namespace Kumibot.App.Models.SportRadar;
+﻿using System.Text.Json.Serialization;
+
+namespace Kumibot.App.Models.SportRadar;
 
 public class Champions
 {
     public class Qualifier
     {
+        [JsonPropertyName("value")]
         public string Value { get; set; }
     }
 
     public class Competitor
     {
+        [JsonPropertyName("id")]
         public string Id { get; set; }
+
+        [JsonPropertyName("name")]
         public string Name { get; set; }
+
+        [JsonPropertyName("abbreviation")]
         public string Abbreviation { get; set; }
+
+        [JsonPropertyName("age_group")]
         public string AgeGroup { get; set; }
+
+        [JsonPropertyName("country")]
         public string Country { get; set; }
+
+        [JsonPropertyName("country_code")]
         public string CountryCode { get; set; }
+
+        [JsonPropertyName("gender")]
         public string Gender { get; set; }
+
+        [JsonPropertyName("qualifier")]
         public Qualifier Qualifier { get; set; }
+
+        [JsonPropertyName("virtual")]
         public bool Virtual { get; set; }
     }
 
     public class WeightClass
     {
+        [JsonPropertyName("description")]
         public string Description { get; set; }
+
+        [JsonPropertyName("competitor")]
         public Competitor Competitor { get; set; }
     }
 
     public class Category
     {
+        [JsonPropertyName("id")]
         public string Id { get; set; }
+
+        [JsonPropertyName("name")]
         public string Name { get; set; }
+
+        [JsonPropertyName("weight_classes")]
         public List<WeightClass> WeightClasses { get; set; }
     }
 
     public class Root
     {
+        [JsonPropertyName("categories")]
         public List<Category> Categories { get; set; }
+
+        [JsonPropertyName("generated_at")]
         public DateTime GeneratedAt { get; set; }
     }
 }
