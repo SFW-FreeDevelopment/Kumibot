2e85ea0846a8a4747a46916827ab85291fa99597
Chris Plummer <37936720+christopherplummer@users.noreply.github.com>
2022-05-07 02:04:24 -0400

    Cp/games command init (#2)
    * games.json added to app and build output
    
    * Game class with nested classes
    
    * GameRepository
    
    * ListGamesCommand
    
    * AddGamesCommand
    
    * DI for GameRepository
    
    Co-authored-by: Christopher Plummer <chrisplummer0194@gmail.com>

---
+++ Kumibot.App/Commands/Games/AddGamesCommand.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Text.Json;
+using Discord.Commands;
+using Kumibot.App.Commands;
+using Kumibot.App.Models.Games;
+using Kumibot.App.Repositories;
+
+public class AddGamesCommand : CommandBase
+{
+    private readonly GameRepository _gameRepository;
+
+    public AddGamesCommand(GameRepository gameRepository)
+    {
+        _gameRepository = gameRepository;
+    }
+    
+    [Command("addgame")]
+    public async Task HandleCommandAsync([Remainder] string remainder)
+    {
+        var commandArgs = remainder.Split(" ");
+        var sb = new StringBuilder();
+        for (var i = 1; i < commandArgs.Length; i++)
+        {
+            sb.Append($"{commandArgs[i]} ");
+        }
+
+        var gameToCreate = new Game { Slug = commandArgs[0], Name = sb.ToString() };
+        var game = _gameRepository.AddGame(gameToCreate);
+        await ReplyAsync($"New game added: {game.Slug} - {game.Name}");
+    }
+}
---
+++ Kumibot.App/Models/Games/Game.cs
@@ -0,0 +1,58 @@
+﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json.Converters;
+
+namespace Kumibot.App.Models.Games;
+
+public class Game
+{
+    public string Name { get; set; }
+    public string Slug { get; set; }
+    public List<League> Leagues { get; set; }
+    public List<Fighter> Fighters { get; set; }
+}
+
+public class League
+{
+    public List<Event> Events { get; set; }
+    public List<Fighter> Champions { get; set; }
+}
+
+public class Event
+{
+    public string Name { get; set; }
+    public List<MatchUp> MatchUps { get; set; }
+
+    public class MatchUp
+    {
+        public Fighter FighterOne { get; set; }
+        public Fighter FighterTwo { get; set; }
+    }
+}
+
+public class Fighter
+{
+    public string Name { get; set; }
+    public List<WeightClass> WeightClasses { get; set; }
+    public int Wins { get; set; }
+    public int Losses { get; set; }
+    public int Draws { get; set; }
+}
+
+[JsonConverter(typeof(StringEnumConverter))]
+public enum WeightClass
+{
+    Strawweight,
+    Flyweight,
+    Batamweight,
+    Featherweight,
+    Lightweight,
+    SuperLightweight,
+    Welterweight,
+    SuperWelterweight,
+    Middleweight,
+    SuperMiddleweight,
+    LightHeavyweight,
+    Cruiserweight,
+    Heavyweight,
+    SuperHeavyweight
+}
