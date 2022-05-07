b8de1d783aa2f940a853696e8e303db9957e5c53
Christopher Plummer <chrisplummer0194@gmail.com>
2022-05-07 11:32:27 -0400

    API client scaffolding

---
+++ Kumibot.App/Clients/SportsDataIOClient.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using RestSharp;
+
+namespace Kumibot.App.Clients;
+
+public class SportsDataIOClient
+{
+    private readonly string _apiKey, _baseUrl;
+    private readonly RestClient _client;
+
+    public SportsDataIOClient(IConfiguration configuration)
+    {
+        _apiKey = configuration["SportDataIOApiKey"];
+        _baseUrl = configuration["SportDataIOBaseUrl"];
+        _client = new RestClient(_baseUrl);
+    }
+    
+    private async Task<TRoot> Get<TRoot>(string route) where TRoot : class
+    {
+        try
+        {
+            var request = new RestRequest($"{_baseUrl}/{route}?key={_apiKey}");
+            var response = await _client.ExecuteAsync<TRoot>(request);
+            return response.IsSuccessful ? response.Data : null;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+}
