f5a0e8f59fca80002ea1ed7681974e76a049d647
Christopher Plummer <chrisplummer0194@gmail.com>
2022-05-07 13:46:39 -0400

    Models for Betting

---
+++ Kumibot.App/Models/Betting/Bet.cs
@@ -0,0 +1,8 @@
+﻿namespace Kumibot.App.Models.Betting;
+
+public class Bet
+{
+    public long Owner { get; set; }
+    public double DollarAmount { get; set; }
+    public string Fighter { get; set; }
+}
---
+++ Kumibot.App/Models/Betting/BettingEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace Kumibot.App.Models.Betting;
+
+public class BettingEvent
+{
+    public string EventTitle { get; set; }
+    public List<Tuple<string, string>> MatchUps { get; set; }
+    public List<Bet> Bets { get; set; }
+}
