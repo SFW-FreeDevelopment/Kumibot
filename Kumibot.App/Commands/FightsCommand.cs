e6765ebf6f772d22d05bf9b10cfa8506b0d4a100
Christopher Plummer <chrisplummer0194@gmail.com>
2022-05-07 12:05:57 -0400

    FightsCommand using new API

--- Kumibot.App/Commands/FightsCommand.cs
+++ Kumibot.App/Commands/FightsCommand.cs
@@ -7,26 +7,30 @@ namespace Kumibot.App.Commands;
 public class FightsCommand : CommandBase
 {
     private readonly SportRadarRepository _repository;
+    private readonly SportsDataIORepository _sportsDataIoRepository;
 
-    public FightsCommand(SportRadarRepository repository)
+    public FightsCommand(SportRadarRepository repository, SportsDataIORepository sportsDataIoRepository)
     {
         _repository = repository;
+        _sportsDataIoRepository = sportsDataIoRepository;
     }
 
     [Command("fights")]
     public async Task HandleCommandAsync()
     {
-        var root = await _repository.GetSeasons();
-        var seasons = root?.Seasons?.OrderBy(s => DateTime.Parse(s.StartDate)).GroupBy(s => s.StartDate)
-            .Select(sg => sg.FirstOrDefault())
-            .Where(s => s?.StartDate != null && DateTime.Parse(s.StartDate) > DateTime.Now);
+        // var root = await _repository.GetSeasons();
+        // var seasons = root?.Seasons?.OrderBy(s => DateTime.Parse(s.StartDate)).GroupBy(s => s.StartDate)
+        //     .Select(sg => sg.FirstOrDefault())
+        //     .Where(s => s?.StartDate != null && DateTime.Parse(s.StartDate) > DateTime.Now);
+
+        var events = await _sportsDataIoRepository.GetEvents();
         
         var sb = new StringBuilder();
-        if (seasons != null)
-            foreach (var season in seasons)
+        if (events != null)
+            foreach (var e in events)
             {
                 sb.Append(
-                    $"- {DateTime.Parse(season.StartDate).ToString("D").Replace("Saturday, ", string.Empty)} | {season.Name}\n");
+                    $"- {e.Day.ToString("D").Replace("Saturday, ", string.Empty)} | {e.Name}\n");
             }
 
         await ReplyAsync($"**Fights List**:\n{sb}");
