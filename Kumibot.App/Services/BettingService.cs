cbf3a5f2c33be25819ec00cd30021f619843ae41
Christopher Plummer <chrisplummer0194@gmail.com>
2022-05-07 13:46:57 -0400

    BettingService

---
+++ Kumibot.App/Services/BettingService.cs
@@ -0,0 +1,35 @@
+﻿using Kumibot.App.Models.Betting;
+
+namespace Kumibot.App.Services;
+
+public class BettingService
+{
+    private BettingEvent BettingEvent { get; set; }
+
+    public bool CreateEvent(BettingEvent bettingEvent)
+    {
+        BettingEvent = bettingEvent;
+        return BettingEvent is not null;
+    }
+
+    public string GetEventTitle()
+    {
+        return BettingEvent.EventTitle;
+    }
+
+    public List<Bet> GetBets()
+    {
+        return BettingEvent.Bets;
+    }
+    
+    public List<Tuple<string, string>> GetMatchUps()
+    {
+        return BettingEvent.MatchUps;
+    }
+
+    public bool AddBet(Bet bet)
+    {
+        BettingEvent.Bets.Add(bet);
+        return BettingEvent.Bets.Contains(bet);
+    }
+}
