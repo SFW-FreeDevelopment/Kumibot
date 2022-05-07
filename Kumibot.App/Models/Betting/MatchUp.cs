7d80ee002cca3e2bd819ed012049bd1bc38f7d50
Christopher Plummer <chrisplummer0194@gmail.com>
2022-05-07 14:29:18 -0400

    MatchUp model

---
+++ Kumibot.App/Models/Betting/MatchUp.cs
@@ -0,0 +1,9 @@
+﻿namespace Kumibot.App.Models.Betting;
+
+public class MatchUp
+{
+    public string FighterOne { get; set; }
+    public string FighterTwo { get; set; }
+    public string Winner { get; set; }
+    public bool Finished { get; set; }
+}
