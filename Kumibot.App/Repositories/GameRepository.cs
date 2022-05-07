02f2c7b7b8989169d81ac005083384e5ea63c825
Christopher Plummer <chrisplummer0194@gmail.com>
2022-05-07 02:02:32 -0400

    GameRepository

---
+++ Kumibot.App/Repositories/GameRepository.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+using Kumibot.App.Models.Games;
+
+namespace Kumibot.App.Repositories;
+
+public class GameRepository
+{
+    private readonly List<Game> _games;
+    private readonly StreamReader _gamesStream;
+    private readonly string _sFile;
+    
+    public GameRepository()
+    {
+        var sCurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;            
+        _sFile = Path.Combine(sCurrentDirectory, @"..\..\..\Data\games.json");
+        _gamesStream = new StreamReader(Path.GetFullPath(_sFile));
+        _games = JsonSerializer.Deserialize<List<Game>>(_gamesStream.ReadToEnd());
+        _gamesStream.Close();
+    }
+
+    public List<Game> GetGames()
+    {
+        return _games;
+    }
+    
+    public Game GetGameBySlug(string slug)
+    {
+        return _games.FirstOrDefault(g => g.Slug.Equals(slug));
+    }
+    
+    public Game AddGame(Game game)
+    {
+        _games.Add(game);
+        File.WriteAllText(_sFile, JsonSerializer.Serialize(_games));
+        return _games.FirstOrDefault(g => g.Slug.Equals(game.Slug));
+    }
+}
