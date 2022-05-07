a35afbb065978ee60713779fb1111fa207bfddfb
Christopher Plummer <chrisplummer0194@gmail.com>
2022-05-07 14:29:06 -0400

    DateTime properties

--- Kumibot.App/Models/Betting/BettingEvent.cs
+++ Kumibot.App/Models/Betting/BettingEvent.cs
@@ -5,4 +5,6 @@ public class BettingEvent
     public string EventTitle { get; set; }
     public List<Tuple<string, string>> MatchUps { get; set; }
     public List<Bet> Bets { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime? UpdatedAt { get; set; }
 }
