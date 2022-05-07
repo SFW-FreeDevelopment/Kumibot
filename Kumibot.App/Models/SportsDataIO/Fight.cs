32f9a79b882c463bf224fdbce7d59a26fad82edb
Christopher Plummer <chrisplummer0194@gmail.com>
2022-05-07 12:33:27 -0400

    New models

---
+++ Kumibot.App/Models/SportsDataIO/Fight.cs
@@ -0,0 +1,18 @@
+﻿namespace Kumibot.App.Models.SportsDataIO;
+
+public class Fight
+{
+    public long FightId { get; set; }
+    public int? Order { get; set; }
+    public string Status { get; set; }
+    public string WeightClass { get; set; }
+    public string CardSegment { get; set; }
+    public string Referee { get; set; }
+    public int Rounds { get; set; }
+    public int ResultClock { get; set; }
+    public int ResultRound { get; set; }
+    public string ResultType { get; set; }
+    public object WinnerId { get; set; }
+    public bool Active { get; set; }
+    public List<Fighter> Fighters { get; set; }
+}
