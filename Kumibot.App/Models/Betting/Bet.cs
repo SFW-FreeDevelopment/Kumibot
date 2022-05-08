5bd44d9eedc1d32d3dc093cc18e92ed0b763e961
Christopher Plummer <chrisplummer0194@gmail.com>
2022-05-07 22:53:28 -0400

    Model and repo updates

--- Kumibot.App/Models/Betting/Bet.cs
+++ Kumibot.App/Models/Betting/Bet.cs
@@ -5,4 +5,5 @@ public class Bet
     public ulong Owner { get; set; }
     public double DollarAmount { get; set; }
     public string Fighter { get; set; }
+    public bool Processed { get; set; }
 }
