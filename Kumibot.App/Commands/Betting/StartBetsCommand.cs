5ad469f81cf313a8dcdbcbe7d62197eb64deed54
Christopher Plummer <chrisplummer0194@gmail.com>
2022-05-09 12:45:54 -0400

    Disabling commands temporarily

--- Kumibot.App/Commands/Betting/StartBetsCommand.cs
+++ Kumibot.App/Commands/Betting/StartBetsCommand.cs
@@ -1,7 +1,7 @@
 ﻿using System.Text;
 using Discord.Commands;
 using Kumibot.App.Clients;
-using Kumibot.App.Models.Betting;
+//using Kumibot.App.Models.Betting;
 using Kumibot.App.Models.SportsDataIO;
 using Kumibot.App.Repositories;
 using Kumibot.App.Services;
@@ -25,83 +25,83 @@ public class StartBetsCommand : CommandBase
     [Command("startbets")]
     public async Task HandleCommandAsync([Remainder] string args)
     {
-        if (args is "current")
-        {
-            var events = await _sportsDataIoRepository.GetEvents();
-            //var currentEvent = events.FirstOrDefault(e => e.Day.Date.Equals(DateTime.Now.Date));
-            var currentEvent = events.FirstOrDefault(e => e.EventId.Equals(239));
-            if (currentEvent is null)
-            {
-                await ReplyAsync("There is no event today. If you need an event, please create a custom event.");
-            }
-            else
-            {
-                var detailedEvent = await _sportsDataIoClient.GetEvent(currentEvent.EventId);
-                var matchUps = GetMatchUpsFromDetailedEventFights(detailedEvent?.Fights).ToList();
-                var eventStarted = _bettingService.StartEvent(new BettingEvent
-                {
-                    EventTitle = currentEvent.Name,
-                    MatchUps = matchUps
-                });
-                if (eventStarted)
-                {
-                    var sb = new StringBuilder($"Betting started for **{currentEvent.Name}**\n\n");
-                    var matchUpCount = matchUps.Count;
-                    if (matchUpCount >= 1)
-                    {
-                        for (var i = 0; i < 5; i++)
-                        {
-                            if (i is 0) sb.Append("**Main Card**\n");
-                            sb.Append($"- {matchUps[i].FighterOne} vs {matchUps[i].FighterTwo}\n");
-                        }
-                    }
-
-                    if (matchUpCount > 5)
-                    {
-                        for (var i = 5; i < 10; i++)
-                        {
-                            if (i is 5) sb.Append("\n**Prelims**\n");
-                            sb.Append($"- {matchUps[i].FighterOne} vs {matchUps[i].FighterTwo}\n");
-                        }
-                    }
-
-                    if (matchUpCount > 10)
-                    {
-                        for (var i = 10; i < 15; i++)
-                        {
-                            if (i is 10) sb.Append("\n**Early Prelims**\n");
-                            sb.Append($"- {matchUps[i].FighterOne} vs {matchUps[i].FighterTwo}\n");
-                        }
-                    }
-
-                    await ReplyAsync(sb.ToString());
-                }
-                else
-                {
-                    await ReplyAsync("Event could not be started.");
-                }
-            }
-        }
+        // if (args is "current")
+        // {
+        //     var events = await _sportsDataIoRepository.GetEvents();
+        //     //var currentEvent = events.FirstOrDefault(e => e.Day.Date.Equals(DateTime.Now.Date));
+        //     var currentEvent = events.FirstOrDefault(e => e.EventId.Equals(239));
+        //     if (currentEvent is null)
+        //     {
+        //         await ReplyAsync("There is no event today. If you need an event, please create a custom event.");
+        //     }
+        //     else
+        //     {
+        //         var detailedEvent = await _sportsDataIoClient.GetEvent(currentEvent.EventId);
+        //         var matchUps = GetMatchUpsFromDetailedEventFights(detailedEvent?.Fights).ToList();
+        //         var eventStarted = _bettingService.StartEvent(new BettingEvent
+        //         {
+        //             EventTitle = currentEvent.Name,
+        //             MatchUps = matchUps
+        //         });
+        //         if (eventStarted)
+        //         {
+        //             var sb = new StringBuilder($"Betting started for **{currentEvent.Name}**\n\n");
+        //             var matchUpCount = matchUps.Count;
+        //             if (matchUpCount >= 1)
+        //             {
+        //                 for (var i = 0; i < 5; i++)
+        //                 {
+        //                     if (i is 0) sb.Append("**Main Card**\n");
+        //                     sb.Append($"- {matchUps[i].FighterOne} vs {matchUps[i].FighterTwo}\n");
+        //                 }
+        //             }
+        //
+        //             if (matchUpCount > 5)
+        //             {
+        //                 for (var i = 5; i < 10; i++)
+        //                 {
+        //                     if (i is 5) sb.Append("\n**Prelims**\n");
+        //                     sb.Append($"- {matchUps[i].FighterOne} vs {matchUps[i].FighterTwo}\n");
+        //                 }
+        //             }
+        //
+        //             if (matchUpCount > 10)
+        //             {
+        //                 for (var i = 10; i < 15; i++)
+        //                 {
+        //                     if (i is 10) sb.Append("\n**Early Prelims**\n");
+        //                     sb.Append($"- {matchUps[i].FighterOne} vs {matchUps[i].FighterTwo}\n");
+        //                 }
+        //             }
+        //
+        //             await ReplyAsync(sb.ToString());
+        //         }
+        //         else
+        //         {
+        //             await ReplyAsync("Event could not be started.");
+        //         }
+        //     }
+        // }
     }
 
     #region Private Methods
 
-    private static IEnumerable<MatchUp> GetMatchUpsFromDetailedEventFights(List<Fight> fights)
-    {
-        var matchUps = new List<MatchUp>();
-        if (fights is null) return matchUps;
-        matchUps.AddRange(from fight in fights
-            let fighters = fight.Fighters
-            select new MatchUp
-            {
-                FighterOneId = fighters[0].FighterId,
-                FighterTwoId = fighters[1].FighterId,
-                FighterOne = $"{fighters[0].FirstName} {fighters[0].LastName}",
-                FighterTwo = $"{fighters[1].FirstName} {fighters[1].LastName}",
-                Position = fight.Order ?? -1
-            });
-        return matchUps;
-    }
+    // private static IEnumerable<MatchUp> GetMatchUpsFromDetailedEventFights(List<Fight> fights)
+    // {
+    //     var matchUps = new List<MatchUp>();
+    //     if (fights is null) return matchUps;
+    //     matchUps.AddRange(from fight in fights
+    //         let fighters = fight.Fighters
+    //         select new MatchUp
+    //         {
+    //             FighterOneId = fighters[0].FighterId,
+    //             FighterTwoId = fighters[1].FighterId,
+    //             FighterOne = $"{fighters[0].FirstName} {fighters[0].LastName}",
+    //             FighterTwo = $"{fighters[1].FirstName} {fighters[1].LastName}",
+    //             Position = fight.Order ?? -1
+    //         });
+    //     return matchUps;
+    // }
 
     #endregion
 }
