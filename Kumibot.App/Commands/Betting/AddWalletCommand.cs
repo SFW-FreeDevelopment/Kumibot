7ede6f14f6b2941b200a11ddb7f74fae09b8e543
Christopher Plummer <chrisplummer0194@gmail.com>
2022-05-09 12:31:16 -0400

    Fix AddWallet

--- Kumibot.App/Commands/Betting/AddWalletCommand.cs
+++ Kumibot.App/Commands/Betting/AddWalletCommand.cs
@@ -1,7 +1,6 @@
-﻿using System.Text;
-using Discord.Commands;
-using Kumibot.App.Models.Betting;
-using Kumibot.App.Repositories;
+﻿using Discord.Commands;
+using Kumibot.Database.Models.Betting;
+using Kumibot.Database.Repositories;
 
 namespace Kumibot.App.Commands.Betting;
 
@@ -18,7 +17,7 @@ public class AddWalletCommand : CommandBase
     public async Task HandleCommandAsync()
     {
         var walletToCreate = new Wallet { Owner = GuildUser.Id, Dollars = 100000.00};
-        var wallet = _walletRepository.AddWallet(walletToCreate);
+        var wallet = await _walletRepository.CreateWallet(walletToCreate);
         await ReplyAsync($"New wallet added for <@{wallet.Owner}>");
     }
 }
