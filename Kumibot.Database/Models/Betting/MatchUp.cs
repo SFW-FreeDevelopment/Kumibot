c57a1e1a2973d6a13380dfd3f0d439fcee37e064
Christopher Plummer <chrisplummer0194@gmail.com>
2022-05-19 12:40:53 -0400

    Model update

--- Kumibot.Database/Models/Betting/MatchUp.cs
+++ Kumibot.Database/Models/Betting/MatchUp.cs
@@ -2,10 +2,12 @@
 
 public class MatchUp
 {
-    public long FighterOneId { get; set; }
-    public long FighterTwoId { get; set; }
+    public long? FighterOneId { get; set; }
+    public long? FighterTwoId { get; set; }
     public string FighterOne { get; set; }
     public string FighterTwo { get; set; }
+    public int FighterOneOdds { get; set; }
+    public int FighterTwoOdds { get; set; }
     public string Winner { get; set; }
     public bool Finished { get; set; }
     public int Position { get; set; }
