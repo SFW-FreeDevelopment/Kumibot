094fef4459dd308ed8bd2b3be3dfa8abb7d44ad5
Christopher Plummer <chrisplummer0194@gmail.com>
2022-05-22 21:00:53 -0400

    CreateBettingEvent slash command and AddMatchUpModal with response

---
+++ Kumibot.App/Interactions/Modals/Responses/AddMatchUpModalResponse.cs
@@ -0,0 +1,64 @@
+﻿using Discord;
+using Discord.Interactions;
+using Kumibot.Database.Models.Betting;
+using Kumibot.Database.Repositories;
+
+namespace Kumibot.App.Interactions.Modals.Responses;
+
+public class AddMatchUpModalResponse : InteractionBase
+{
+    private readonly BettingEventRepository _bettingEventRepository;
+
+    public AddMatchUpModalResponse(BettingEventRepository bettingEventRepository)
+    {
+        _bettingEventRepository = bettingEventRepository;
+    }
+
+    [ModalInteraction("add_match_up")]
+    public async Task ModalResponse(AddMatchUpModal modal)
+    {
+        var bettingEvent = await _bettingEventRepository.GetBettingEventById(modal.BettingEventId);
+        var position = bettingEvent.MatchUps?.Max(x => x.Position) + 1 ?? 1;
+        bettingEvent.MatchUps?.Add(new MatchUp
+        {
+            FighterOne = modal.FighterOneName,
+            FighterTwo = modal.FighterTwoName,
+            FighterOneOdds = GetOdds(modal.FighterOneOdds),
+            FighterTwoOdds = GetOdds(modal.FighterTwoOdds),
+            Position = position
+        });
+        var updatedEvent = await _bettingEventRepository.UpdateBettingEvent(bettingEvent.Id, bettingEvent);
+        if (updatedEvent is not null)
+        {
+            // Build the message to send.
+            var message =
+                $"{Mention} added Match-up to {bettingEvent.EventTitle}. {modal.FighterOneName} (Odds: {modal.FighterOneOdds}) vs {modal.FighterTwoName} (Odds: {modal.FighterTwoOdds})";
+
+            // Specify the AllowedMentions so we don't actually ping everyone.
+            AllowedMentions mentions = new();
+            mentions.AllowedTypes = AllowedMentionTypes.Users;
+
+            // Respond to the modal.
+            await ReplyAsync(message, allowedMentions: mentions);
+        }
+    }
+
+    private static int GetOdds(string oddsString)
+    {
+        bool isInt;
+        if (oddsString.Contains('-'))
+        {
+            isInt = int.TryParse(oddsString, out var negativeOdds);
+            if (isInt) return negativeOdds;
+        }
+
+        if (oddsString.Contains('+'))
+        {
+            isInt = int.TryParse(oddsString.Replace("+", string.Empty), out var positiveOdds);
+            if (isInt) return positiveOdds;
+        }
+
+        isInt = int.TryParse(oddsString, out var odds);
+        return isInt ? odds : 0;
+    }
+}
--- Kumibot.App/Interactions/SlashCommands/CreateBettingEventSlashCommand.cs
+++ Kumibot.App/Interactions/SlashCommands/CreateBettingEventSlashCommand.cs
@@ -1,10 +1,49 @@
-﻿using Discord.Interactions;
+﻿using Discord;
+using Discord.WebSocket;
 using Kumibot.App.Interactions.Modals;
+using Kumibot.Database.Models.Betting;
+using Kumibot.Database.Repositories;
 
 namespace Kumibot.App.Interactions.SlashCommands;
 
 public class CreateBettingEventSlashCommand : InteractionBase
 {
-    [SlashCommand("createbettingevent", "Creates a betting event for server members to place bets.")]
-    public async Task CreateBettingEvent() => await Interaction.RespondWithModalAsync<CreateBettingEventModal>("create_betting_form");
+    private readonly BettingEventRepository _bettingEventRepository;
+    private readonly DiscordSocketClient _discordClient;
+
+    public CreateBettingEventSlashCommand(BettingEventRepository bettingEventRepository, DiscordSocketClient discordClient)
+    {
+        _bettingEventRepository = bettingEventRepository;
+        _discordClient = discordClient;
+    }
+
+    [Discord.Interactions.SlashCommand("createbettingevent", "Creates a betting event for server members to place bets. Adds one match-up to start.")]
+    public async Task CreateBettingEvent(string eventTitle)
+    {
+        var newEvent = await _bettingEventRepository.CreateBettingEvent(new BettingEvent
+        {
+            EventTitle = eventTitle
+        });
+        if (newEvent is not null)
+        {
+            var modalBuilder = new ModalBuilder()
+                .WithTitle($"Add Match-Up for {newEvent.EventTitle}")
+                .WithCustomId("add_match_up")
+                .AddTextInput("Betting Event Id", "betting_event_id", TextInputStyle.Short, "", null, null, true,
+                    newEvent.Id)
+                .AddTextInput(
+                    $"Fighter 1 Name",
+                    "fighter_1_name", placeholder: "Fighter One", required: true)
+                .AddTextInput("Fighter 1 Odds",
+                    "fighter_1_odds", placeholder: "+100")
+                .AddTextInput(
+                    "Fighter 2 Name",
+                    "fighter_2_name", placeholder: "Fighter Two", required: true)
+                .AddTextInput("Fighter 2 Odds",
+                    "fighter_2_odds", placeholder: "-100");
+
+            await RespondWithModalAsync(modalBuilder.Build());
+            await Interaction.RespondAsync($"{Mention} has created a betting event for {eventTitle}!");
+        }
+    }
 }
