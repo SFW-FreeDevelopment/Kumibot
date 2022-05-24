53683786e4a6ab005120adccd438bb364432d62c
Christopher Plummer <chrisplummer0194@gmail.com>
2022-05-23 23:47:46 -0400

    Remove unused components

--- Kumibot.App/Interactions/Modals/PlaceBetFormModal.cs
+++
@@ -1,9 +0,0 @@
-﻿using Discord.Interactions;
-
-namespace Kumibot.App.Interactions.Modals;
-
-[Group("kumibot", "Kumibot commands")]
-public class PlaceBetFormModal : IModal
-{
-    public string Title => "Place Bets";
-}
