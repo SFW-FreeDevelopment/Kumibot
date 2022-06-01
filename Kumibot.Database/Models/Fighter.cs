b75adb68e7f336dbefb057e034933beee73bd66e
Christopher Plummer <chrisplummer0194@gmail.com>
2022-06-01 15:04:53 -0400

    Model updates

--- Kumibot.Database/Models/Fighter.cs
+++ Kumibot.Database/Models/Fighter.cs
@@ -3,12 +3,4 @@
 public class Fighter : BaseResource
 {
     public string Name { get; set; }
-
-    public Fighter()
-    {
-        Id = Guid.NewGuid().ToString();
-        CreatedAt = DateTime.Now;
-        UpdatedAt = DateTime.Now;
-        Version = 1;
-    }
 }
