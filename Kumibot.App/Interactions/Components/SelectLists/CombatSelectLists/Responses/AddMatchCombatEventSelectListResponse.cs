0a8856c790d45ae471ee68fa11d59d8c93da1fa8
Christopher Plummer <chrisplummer0194@gmail.com>
2022-06-10 00:07:33 -0400

    Command and initial component for AddMatch

---
+++ Kumibot.App/Interactions/Components/SelectLists/CombatSelectLists/Responses/AddMatchCombatEventSelectListResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace Kumibot.App.Interactions.Components.SelectLists.CombatSelectLists.Responses;
+//TODO: Add logic for response
+public class AddMatchCombatEventSelectListResponse
+{
+    
+}
---
+++ Kumibot.App/Interactions/SlashCommands/CombatSlashCommands/AddMatchSlashCommand.cs
@@ -0,0 +1,32 @@
+﻿using Discord.Interactions;
+using Kumibot.App.Helpers;
+using Kumibot.App.Services;
+using Kumibot.Database.Models.Combat;
+
+namespace Kumibot.App.Interactions.SlashCommands.CombatSlashCommands;
+
+public class AddMatchSlashCommand: InteractionBase
+{
+    private readonly CombatService _combatService;
+
+    public AddMatchSlashCommand(CombatService combatService)
+    {
+        _combatService = combatService;
+    }
+
+    [SlashCommand("addsinglesmatch", "Adds a 1 vs 1 match to a combat event")]
+    public async Task AddSinglesMatch()
+    {
+        var combatEvents = await _combatService.GetByDiscordOwner(User.Id);
+        if (combatEvents.Count > 0)
+        {
+            var selectListOptions = combatEvents.Where(x => x.Status.Equals(CombatEventStatus.Created)).Select(x => (x.EventTitle, x.Id));
+            var combatEventSelectList = SelectListHelper.GetSelectList(Constants.AddMatchCombatEventSelectListId, selectListOptions.ToDictionary(x => x.EventTitle, x=> x.Id));
+            await RespondAsync("Select the event to add the match to:", components: combatEventSelectList.Build());
+        }
+        else
+        {
+            await ReplyAsync($"Could not find any combat events owned by {Mention}.");
+        }
+    }
+}
