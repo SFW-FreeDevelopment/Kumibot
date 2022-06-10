e748965f016e77a8ef21f407293d56eca5726957
Christopher Plummer <chrisplummer0194@gmail.com>
2022-06-09 23:16:05 -0400

    Add singles tournament create command

--- Kumibot.App/Interactions/SlashCommands/CombatSlashCommands/CreateSinglesTournamentSlashCommand.cs
+++ Kumibot.App/Interactions/SlashCommands/CombatSlashCommands/CreateSinglesTournamentSlashCommand.cs
@@ -1,10 +1,12 @@
 ﻿using Discord.Interactions;
 using Discord.WebSocket;
+using Kumibot.App.Interactions.Components.Modals.CombatModals;
+using Kumibot.Database.Models.Combat;
 using Kumibot.Database.Repositories.Combat;
 
 namespace Kumibot.App.Interactions.SlashCommands.CombatSlashCommands;
 //TODO: Automatically add betting event when CombatEvent is created
-public class CreateSinglesTournamentSlashCommand
+public class CreateSinglesTournamentSlashCommand : InteractionBase
 {
     private readonly CombatEventRepository _combatEventRepository;
     private readonly DiscordSocketClient _discordClient;
@@ -18,6 +20,18 @@ public class CreateSinglesTournamentSlashCommand
     [SlashCommand("createsinglestournament", "Creates a 1 vs 1 tournament")]
     public async Task CreateSinglesTournament(string eventTitle)
     {
-        
+        var eventToCreate = new CombatEvent
+        {
+            EventTitle = eventTitle,
+            Type = CombatEventType.Tournament,
+            DiscordOwner = User.Id
+        };
+        var newEvent = await _combatEventRepository.Create(eventToCreate);
+        if (newEvent is not null)
+        {
+            await ReplyAsync($"{Mention} has created a 1 vs 1 tournament: {eventTitle}!");
+            var modalBuilder = AddSinglesMatchModal.GetAddSinglesMatchModal(newEvent.Id);
+            await RespondWithModalAsync(modalBuilder.Build());
+        }
     }
 }
