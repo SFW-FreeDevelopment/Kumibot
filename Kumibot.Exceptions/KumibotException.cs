4351e18f4059db96df356a2b93c78c597708d459
Christopher Plummer <chrisplummer0194@gmail.com>
2022-06-11 11:58:32 -0400

    Exceptions library

---
+++ Kumibot.Exceptions/KumibotException.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace Kumibot.Exceptions;
+
+public class KumibotException : Exception
+{
+    public HttpStatusCode StatusCode { get; set; }
+    
+    public KumibotException () {}
+
+    public KumibotException(HttpStatusCode statusCode)
+    {
+        StatusCode = statusCode;
+    }
+}
