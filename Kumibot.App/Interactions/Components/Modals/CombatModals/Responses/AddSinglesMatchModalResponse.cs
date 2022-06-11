62ca8ab7e991b5a1f6df27c8ebf99c4ea6313f6d
Christopher Plummer <chrisplummer0194@gmail.com>
2022-06-11 11:23:53 -0400

    Using service

--- Kumibot.App/Interactions/Components/Modals/CombatModals/Responses/AddSinglesMatchModalResponse.cs
+++ Kumibot.App/Interactions/Components/Modals/CombatModals/Responses/AddSinglesMatchModalResponse.cs
@@ -7,19 +7,19 @@ namespace Kumibot.App.Interactions.Components.Modals.CombatModals.Responses;
 
 public class AddSinglesMatchModalResponse : InteractionBase
 {
-    private readonly CombatEventRepository _combatEventRepository;
+    private readonly CombatService _combatService;
     private readonly FighterService _fighterService;
 
-    public AddSinglesMatchModalResponse(CombatEventRepository combatEventRepository, FighterService fighterService)
+    public AddSinglesMatchModalResponse(CombatService combatService, FighterService fighterService)
     {
-        _combatEventRepository = combatEventRepository;
+        _combatService = combatService;
         _fighterService = fighterService;
     }
     //TODO: Look into combatEvent not updating in DB
     [ModalInteraction("add_singles_match")]
     public async Task ModalResponse(AddSinglesMatchModal modal)
     {
-        var combatEvent = await _combatEventRepository.GetById(modal.NeededValuesId);
+        var combatEvent = await _combatService.GetById(modal.NeededValuesId);
         var fighterOne = await _fighterService.Create(new Fighter { Name = modal.FighterOneName });
         var fighterTwo = await _fighterService.Create(new Fighter { Name = modal.FighterTwoName });
         if (fighterOne.Equals(null) || fighterTwo.Equals(null)) await ReplyAsync("Can't create match.");
@@ -32,7 +32,7 @@ public class AddSinglesMatchModalResponse : InteractionBase
                 FighterTwoId = fighterTwo.Id,
                 Position =  maxPosition < 1 ? 1 : maxPosition + 1
             });
-            await _combatEventRepository.Update(combatEvent.Id, combatEvent);
+            await _combatService.Update(combatEvent.Id, combatEvent);
             await ReplyAsync($"{Mention} added a match to {combatEvent.EventTitle}. {fighterOne.Name} vs {fighterTwo.Name}");
         }
     }
