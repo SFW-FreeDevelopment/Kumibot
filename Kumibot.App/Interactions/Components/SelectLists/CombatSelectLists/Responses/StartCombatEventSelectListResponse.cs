c3513cfabe8e2b70ed3cf653395dd1525b518e2a
Christopher Plummer <chrisplummer0194@gmail.com>
2022-06-11 12:46:28 -0400

    StartCombatEvent select list response logic

--- Kumibot.App/Interactions/Components/SelectLists/CombatSelectLists/Responses/StartCombatEventSelectListResponse.cs
+++ Kumibot.App/Interactions/Components/SelectLists/CombatSelectLists/Responses/StartCombatEventSelectListResponse.cs
@@ -1,6 +1,63 @@
-﻿namespace Kumibot.App.Interactions.Components.SelectLists.CombatSelectLists.Responses;
+﻿using Discord;
+using Discord.Interactions;
+using Kumibot.App.Interactions.Components.Modals.BettingModals;
+using Kumibot.App.Services;
+using Kumibot.Database.Models.Betting;
+using Kumibot.Database.Models.Combat;
+using Kumibot.Exceptions;
 
-public class StartCombatEventSelectListResponse
+namespace Kumibot.App.Interactions.Components.SelectLists.CombatSelectLists.Responses;
+
+public class StartCombatEventSelectListResponse : InteractionBase
 {
+    private readonly BettingService _bettingService;
+    private readonly CombatService _combatService;
     
+    public StartCombatEventSelectListResponse(BettingService bettingService, CombatService combatService)
+    {
+        _bettingService = bettingService;
+        _combatService = combatService;
+    }
+
+    [ComponentInteraction(Constants.StartCombatEventCombatEventSelectListId)]
+    public async Task ComponentResponse()
+    {
+        try
+        {
+            var interaction = (IComponentInteraction)Interaction;
+            var combatEventId = interaction.Data.Values.FirstOrDefault();
+            var combatEvent =
+                await _combatService.GetById(combatEventId ?? string.Empty);
+            if (!combatEvent.Equals(null))
+            {
+                var bettingEvent = await _bettingService.GetByCombatEventId(combatEvent.Id);
+                if (!bettingEvent.Equals(null))
+                {
+                    bettingEvent.IsActive = true;
+                    combatEvent.Status = CombatEventStatus.Running;
+                    await _combatService.Update(combatEvent.Id, combatEvent);
+                    await _bettingService.Update(bettingEvent.Id, bettingEvent);
+                    await ReplyAsync($"{combatEvent.EventTitle} has started!");
+                }
+                else
+                {
+                    await ReplyAsync("Could not start event.");
+                }
+            }
+            else
+            {
+                await ReplyAsync("Could not start event.");
+            }
+        }
+        catch (KumibotException e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
 }
