aec6aa1d226cda09a331e6eae5d9fe9564eb563a
Christopher Plummer <chrisplummer0194@gmail.com>
2022-06-11 11:45:00 -0400

    Create betting event when singles fight card is created

--- Kumibot.App/Interactions/SlashCommands/CombatSlashCommands/CreateSinglesFightCardSlashCommand.cs
+++ Kumibot.App/Interactions/SlashCommands/CombatSlashCommands/CreateSinglesFightCardSlashCommand.cs
@@ -1,6 +1,7 @@
 ﻿using Discord.Interactions;
 using Kumibot.App.Interactions.Components.Modals.CombatModals;
 using Kumibot.App.Services;
+using Kumibot.Database.Models.Betting;
 using Kumibot.Database.Models.Combat;
 
 namespace Kumibot.App.Interactions.SlashCommands.CombatSlashCommands;
@@ -9,24 +10,31 @@ public class CreateSinglesFightCardSlashCommand : InteractionBase
 {
     
     private readonly CombatService _combatService;
+    private readonly BettingService _bettingService;
 
-    public CreateSinglesFightCardSlashCommand(CombatService combatService)
+    public CreateSinglesFightCardSlashCommand(CombatService combatService, BettingService bettingService)
     {
         _combatService = combatService;
+        _bettingService = bettingService;
     }
     
     [SlashCommand("createsinglesfightcard", "Creates a 1 vs 1 fight card.")]
     public async Task CreateSinglesFightCard(string eventTitle)
     {
-        var eventToCreate = new CombatEvent
+        var combatEventToCreate = new CombatEvent
         {
             EventTitle = eventTitle,
             Type = CombatEventType.SingleExhibition,
             DiscordOwner = User.Id
         };
-        var newEvent = await _combatService.Create(eventToCreate);
+        var newEvent = await _combatService.Create(combatEventToCreate);
         if (newEvent is not null)
         {
+            var bettingEventToCreate = new BettingEvent
+            {
+                CombatEventId = newEvent.Id
+            };
+            await _bettingService.Create(bettingEventToCreate);
             await ReplyAsync($"{Mention} has created a 1 vs 1 fight card: {eventTitle}!");
             var modalBuilder = AddSinglesMatchModal.GetAddSinglesMatchModal(newEvent.Id);
             await RespondWithModalAsync(modalBuilder.Build());
