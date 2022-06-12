0d590890f454a65ac938ead28ec78b7a7eb24a38
Christopher Plummer <chrisplummer0194@gmail.com>
2022-06-11 22:12:33 -0400

    SelectList responses

--- Kumibot.App/Interactions/Components/SelectLists/BettingSelectLists/Responses/AddMatchBettingInfoMatchSelectListResponse.cs
+++ Kumibot.App/Interactions/Components/SelectLists/BettingSelectLists/Responses/AddMatchBettingInfoMatchSelectListResponse.cs
@@ -1,6 +1,43 @@
-﻿namespace Kumibot.App.Interactions.Components.SelectLists.BettingSelectLists.Responses;
+﻿using Discord;
+using Discord.Interactions;
+using Kumibot.App.Helpers;
+using Kumibot.App.Interactions.Components.Modals.BettingModals;
+using Kumibot.App.Services;
 
-public class AddMatchBettingInfoMatchSelectListResponse
+namespace Kumibot.App.Interactions.Components.SelectLists.BettingSelectLists.Responses;
+
+public class AddMatchBettingInfoMatchSelectListResponse : InteractionBase
 {
-    
+    private readonly CombatService _combatService;
+    private readonly FighterService _fighterService;
+
+    public AddMatchBettingInfoMatchSelectListResponse(CombatService combatService, FighterService fighterService)
+    {
+        _combatService = combatService;
+        _fighterService = fighterService;
+    }
+
+    [ComponentInteraction(Constants.AddMatchBettingInfoMatchSelectListId)]
+    public async Task ComponentResponse()
+    {
+        var interaction = (IComponentInteraction)Interaction;
+        var splitValues = interaction.Data.Values.FirstOrDefault()?.Split("_");
+        if (splitValues != null)
+        {
+            var combatEventId = splitValues[0];
+            var matchRound = int.Parse(splitValues[1]);
+            var matchPosition = int.Parse(splitValues[2]);
+            var combatEvent =
+                await _combatService.GetById(combatEventId);
+            var match = combatEvent.Matches.FirstOrDefault(x => x.Round.Equals(matchRound) && x.Position.Equals(matchPosition));
+            var selectListOptions = new Dictionary<string, string>();
+            var fighterOne = await _fighterService.GetById(match?.FighterOneId);
+            var fighterTwo = await _fighterService.GetById(match?.FighterTwoId);
+            if (match != null)
+            {
+                var modalBuilder = AddMatchBettingInfoModal.GetAddMatchBettingInfoModal(combatEvent.Id, match, fighterOne.Name, fighterTwo.Name);
+                await RespondWithModalAsync(modalBuilder.Build());
+            }
+        }
+    }
 }
--- Kumibot.App/Interactions/Components/SelectLists/BettingSelectLists/Responses/PlaceBetMatchSelectListResponse.cs
+++ Kumibot.App/Interactions/Components/SelectLists/BettingSelectLists/Responses/PlaceBetMatchSelectListResponse.cs
@@ -1,6 +1,45 @@
-﻿namespace Kumibot.App.Interactions.Components.SelectLists.BettingSelectLists.Responses;
+﻿using Discord;
+using Discord.Interactions;
+using Kumibot.App.Helpers;
+using Kumibot.App.Services;
+
+namespace Kumibot.App.Interactions.Components.SelectLists.BettingSelectLists.Responses;
 //TODO: Add code for response
-public class PlaceBetMatchSelectListResponse
+public class PlaceBetMatchSelectListResponse : InteractionBase
 {
-    
+    private readonly CombatService _combatService;
+    private readonly FighterService _fighterService;
+
+    public PlaceBetMatchSelectListResponse(CombatService combatService, FighterService fighterService)
+    {
+        _combatService = combatService;
+        _fighterService = fighterService;
+    }
+
+    [ComponentInteraction(Constants.PlaceBetMatchSelectListId)]
+    public async Task ComponentResponse()
+    {
+        var interaction = (IComponentInteraction)Interaction;
+        var splitValues = interaction.Data.Values.FirstOrDefault()?.Split("_");
+        if (splitValues != null)
+        {
+            var combatEventId = splitValues[0];
+            var matchRound = int.Parse(splitValues[1]);
+            var matchPosition = int.Parse(splitValues[2]);
+            var combatEvent =
+                await _combatService.GetById(combatEventId);
+            var match = combatEvent.Matches.FirstOrDefault(x => x.Round.Equals(matchRound) && x.Position.Equals(matchPosition));
+            var selectListOptions = new Dictionary<string, string>();
+            var fighterOne = await _fighterService.GetById(match?.FighterOneId);
+            var fighterTwo = await _fighterService.GetById(match?.FighterTwoId);
+            if (match != null)
+            {
+                selectListOptions.Add(fighterOne.Name, $"{combatEvent.Id}_{match.Round}_{match.Position}");
+                selectListOptions.Add(fighterTwo.Name, $"{combatEvent.Id}_{match.Round}_{match.Position}");
+            }
+
+            var fighterSelectList = SelectListHelper.GetSelectList(Constants.PlaceBetFighterSelectListId, selectListOptions);
+            await RespondAsync("Select the fighter to bet on:", components: fighterSelectList.Build());
+        }
+    }
 }
