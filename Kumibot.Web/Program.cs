35e0e31b727aaba37204b347ba5a5a974bf597da
Christopher Plummer <chrisplummer0194@gmail.com>
2022-07-02 15:51:47 -0400

    Wireup

--- Kumibot.Web/Program.cs
+++ Kumibot.Web/Program.cs
@@ -1,6 +1,13 @@
+using Kumibot.Database.Models.Betting;
+using Kumibot.Database.Models.Combat;
+using Kumibot.Database.Repositories.Betting;
+using Kumibot.Database.Repositories.Combat;
+using Kumibot.Database.Repositories.Gaming;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Kumibot.Web.Data;
+using Kumibot.Web.Services;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +15,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddScoped<IMongoClient, MongoClient>(_ =>
+        new MongoClient(
+            MongoClientSettings.FromConnectionString(builder.Configuration["MongoDatabaseConnectionString"])))
+    .AddScoped<GameRepository>()
+    .AddScoped<WalletRepository>()
+    .AddScoped<BettingEventRepository>()
+    .AddScoped<CombatEventRepository>()
+    .AddScoped<FighterRepository>()
+    .AddScoped<ICombatService<BettingEvent>, BettingService>()
+    .AddScoped<ICombatService<CombatEvent>, EventService>()
+    .AddScoped<ICombatService<Fighter>, FighterService>();
 
 var app = builder.Build();
 
