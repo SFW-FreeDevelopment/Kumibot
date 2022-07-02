8313cd0b16163ff0cbe0d711de30eaaf043417f2
Christopher Plummer <chrisplummer0194@gmail.com>
2022-07-02 15:30:35 -0400

    Services init

---
+++ Kumibot.Web/Services/ICombatService.cs
@@ -0,0 +1,12 @@
+﻿using Kumibot.Database.Models;
+
+namespace Kumibot.Web.Services;
+
+public interface ICombatService<T> where T : BaseResource
+{
+    public Task<List<T>> GetAll();
+    public Task<T> GetById(string id);
+    public Task<T> Create(T data);
+    public Task<T> Update(string id, T data);
+    public Task Delete(string id);
+}
